2e103f5b1125e1e2fb56ecfe460c1f8508a878b0
kelvin.tryanto <kelvin.tryanto@gmail.com>
2019-03-15 21:09:05 +0700

    profil gokil

--- profile.php
+++ profile.php
@@ -1,54 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Twittie</title>
-
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-    
-</head>
-<body> 
-    <nav class="navbar fixed-top navbar-expand-md custom-navbar navbar-dark">
-       <img class="navbar-brand" src="img/twittie.png" id="logo_custom" width="10%" style="width: 3%;height: 13%" alt="logo">
-       <button class="navbar-toggler navbar-toggler-right custom-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon "></span>
-    </button>
-    <div class="collapse navbar-collapse " id="collapsibleNavbar">
-      <ul class="navbar-nav ml-auto ">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php"><b>Home</b></a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" href="#"><b>About</b></a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" href="logout.php"><b>Logout</b></a>
-      </li>
-  </ul>
-</div>  
-</nav> 
-<div class="container mt-lg-5">
-    <div class="row">
-        <div class="col-md-6">
-             <br>
-             <br>
-             <br>
-
-        </div>
-
-        <div class="col-md-6">
-            <!-- isi dengan sesuatu di sini -->
-        </div>
-
-    </div>
-</div>
-
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -58,30 +10,85 @@
   <link rel="stylesheet" type="text/css" href="css/profile.css">
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <link rel="stylesheet" href="font-awesome/css/all.min.css">
-
-
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
+
 <body>
   <div class="container">
-    <ul class="twitter" role="navigation">
-      <li>
-        <a href="profile.php">
-          <div class="link"><i class="fa fa-home"></i>Profile</div>
-        </a>
-      </li>
-      <li>
-        <a href="login.php">
-          <div class="link"><i class="fas fa-sign-out-alt"></i>Logout</div>
-        </a>
-      </li>
-      <!-- <li><i class="fa fa-bell"></i>Notifications</li> -->
-      <!-- <li><i class="fa fa-envelope"></i>Messages</li> -->
-      <li class="twitter__bird"><i class="fab fa-twitter"></i></li>
-      <li><form><input placeholder=" search twitter"/> <i class="fa fa-search"></i></form></li>
-      <li><i class="fa fa-user-circle"></i></li>
-      <li><button class="btn"> Tweet </button></li>
-    </ul>
+    <div class="row">
+      <ul class="twitter" role="navigation">
+        <li>
+          <a href="profile.php">
+            <div class="link"><i class="fa fa-home"></i>Profile</div>
+          </a>
+        </li>
+        <li>
+          <a href="login.php">
+            <div class="link"><i class="fas fa-sign-out-alt"></i>Logout</div>
+          </a>
+        </li>
+        <!-- <li><i class="fa fa-bell"></i>Notifications</li> -->
+        <!-- <li><i class="fa fa-envelope"></i>Messages</li> -->
+        <li class="twitter__bird"><i class="fab fa-twitter"></i></li>
+        <li><form><input placeholder=" search twitter"/> <i class="fa fa-search"></i></form></li>
+        <li><i class="fa fa-user-circle"></i></li>
+        <li><button class="btn"> Tweet </button></li>
+      </ul>
+    </div>
+
+    <!-- TEMPAT PROFIL -->
+    <div class="row col-sm-12">
+      <div style="padding-right: 30px; padding-left: 30px; text-align: center;" class="col-3">
+        <div style="height: 150px; background: #666; border-radius: 10px" class="panel panel-default">
+         <div class="profpic">
+          <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+        </div><!-- /thumbnail -->
+        <h4><strong>@selectusername</strong></h4>
+      </div>
+    </div>
+
+
+    <div class="row col-9">
+      <!-- TEMPAT COMMENT -->
+      <div class="row">
+        <div class="col-sm-12">
+          <h3>User Comment Example</h3>
+        </div><!-- /col-sm-12 -->
+      </div><!-- /row -->
+
+      <!-- REPEAT SELECT COMMENT USER DI SINI -->
+      <div class="row col-11">
+        <div class="col-sm-2">
+          <div class="thumbnail">
+            <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+          </div><!-- /thumbnail -->
+        </div><!-- /col-sm-1 -->
+
+        <div style="padding: 0;" class="col-sm-10">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong>@selectusername</strong> <span class="text-muted">commented 5 days ago</span>
+            </div>
+            <div class="panel-body">
+              Select comment from comment table for each user
+            </div><!-- /panel-body -->
+          </div><!-- /panel panel-default -->
+        </div><!-- /col-sm-5 -->
+      </div><!-- /row col-sm-10-->
+
+      <div id="reply" class="row col-sm-1">
+        <ul>
+          <li><i class="fas fa-reply"></i></li>
+          <li><i class="fas fa-trash"></i></li>
+        </ul>
+      </div>
+
+    </div> <!-- /row col-sm-9 -->
   </div>
+</div><!-- /container -->
 </body>
 </html>
---
+++ twittie.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2019 at 12:15 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `twittie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` int(30) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `regdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `mobile`, `username`, `password`, `regdate`) VALUES
+(5, 'Dominic', 'Reinaldo', 'dominic.reinaldo11@gmail.com', 2147483647, 'dominic', '850ab44cce67bf2c5457c6dca6181d60', '2019-03-12 17:56:18');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
