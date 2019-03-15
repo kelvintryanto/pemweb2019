6fcc87b3dc384ff7188af917753eb953e347ef1d
dominic <dominic.reinaldo11@gmail.com>
2019-03-15 17:32:51 +0700

    v.1dom

---
+++ login.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+include('config.php');
+error_reporting(0);
+if(isset($_POST['login']))
+{
+  $username=$_POST['username'];
+  $password=md5($_POST['password']);
+  $sql ="SELECT username,password,firstname FROM users WHERE username=:username and password=:password";
+  $query= $dbh -> prepare($sql);
+  $query-> bindParam(':username', $username, PDO::PARAM_STR);
+  $query-> bindParam(':password', $password, PDO::PARAM_STR);
+  $query-> execute();
+  $results=$query->fetchAll(PDO::FETCH_OBJ);
+  if($query->rowCount() > 0)
+  {
+    $_SESSION['login']=$_POST['username'];
+    $_SESSION['fname']=$results->firstname;
+    $currentpage=$_SERVER['REQUEST_URI'];
+    echo "<script type='text/javascript'> document.location = 'profile.php'; </script>";
+  } else{
+    
+    echo "<script>alert('Invalid Username or Password');</script>";
+
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Twittie</title>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    
+</head>
+<body> 
+    <nav class="navbar fixed-top navbar-expand-md custom-navbar navbar-dark">
+       <img class="navbar-brand" src="img/twittie.png" id="logo_custom" width="10%" style="width: 3%;height: 13%" alt="logo">
+       <button class="navbar-toggler navbar-toggler-right custom-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon "></span>
+    </button>
+    <div class="collapse navbar-collapse " id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto ">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php"><b>Home</b></a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="#"><b>About</b></a>
+      </li>
+  </ul>
+</div>  
+</nav> 
+<div class="container mt-lg-5">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="img/twittie.png" style="width: 5%; height: 15%"> <a href="index.php">Home</a>
+            <h4><b>Log in to Twittie</b></h4>
+
+            <form method="POST">
+
+                <div class="form-group">
+                    <input class="form-control" type="text" name="username" placeholder="Username" required />
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="password" placeholder="Password" required />
+                </div>
+
+                <input type="submit" class="btn btn-primary" name="login" value="Submit" />
+                <label><input type="checkbox"> Remember me</label>
+
+            </form>
+            <p>New to Twittie? <a href="register.php">Sign up now</a></p>   
+        </div>
+
+        <div class="col-md-6">
+            <!-- isi dengan sesuatu di sini -->
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,144 @@
+<?php
+//error_reporting(0);
+session_start();
+include('config.php');
+error_reporting(0);
+if(isset($_POST['signup']))
+{
+  $fname=$_POST['firstname'];
+  $lname=$_POST['lastname'];
+  $email=$_POST['emailid']; 
+  $mobile=$_POST['mobileno'];
+  $username=$_POST['username'];
+  $password=md5($_POST['password']); 
+  $sql="INSERT INTO  users(firstname,lastname,email,mobile,username,password) VALUES(:fname,:lname,:email,:mobile,:username,:password)";
+  $query = $dbh->prepare($sql);
+  $query->bindParam(':fname',$fname,PDO::PARAM_STR);
+  $query->bindParam(':lname',$lname,PDO::PARAM_STR);
+  $query->bindParam(':email',$email,PDO::PARAM_STR);
+  $query->bindParam(':mobile',$mobile,PDO::PARAM_STR);
+  $query->bindParam(':username',$username,PDO::PARAM_STR);
+  $query->bindParam(':password',$password,PDO::PARAM_STR);
+  $query->execute();
+  $lastInsertId = $dbh->lastInsertId();
+  if($lastInsertId)
+  {
+    echo "<script>alert('Registration successfull. Now you can login');document.location='login.php'</script>";
+  }
+  else 
+  {
+    echo "<script>alert('Something went wrong. Please try again');</script>";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Twittie</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="css/login.css"/>
+</head>
+<body>  
+    <nav class="navbar fixed-top navbar-expand-md custom-navbar navbar-dark">
+       <img class="navbar-brand" src="img/twittie.png" id="logo_custom" width="10%" style="width: 3%;height: 13%" alt="logo">
+       <button class="navbar-toggler navbar-toggler-right custom-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon "></span>
+    </button>
+    <div class="collapse navbar-collapse " id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto ">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php"><b>Home</b></a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="#"><b>About</b></a>
+      </li>
+  </ul>
+</div>  
+</nav> 
+
+
+<div class="container-fluid">
+    <div class="container">
+        <div class="formBox">
+            <form method="POST" name="signup" onsubmit="return valid();">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h1>Register</h1>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="inputBox ">
+                            <div class="inputText">First Name</div>
+                            <input type="text" name="firstname" class="input">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="inputBox">
+                            <div class="inputText">Last Name</div>
+                            <input type="text" name="lastname" class="input">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="inputBox">
+                            <div class="inputText">Email</div>
+                            <input type="text" name="emailid" class="input">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="inputBox">
+                            <div class="inputText">Mobile</div>
+                            <input type="text" name="mobileno" class="input">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="inputBox">
+                            <div class="inputText">Username</div>
+                            <input type="text" name="username" class="input">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="inputBox">
+                            <div class="inputText">New Password</div>
+                            <input type="password" name="password" class="input">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <input type="submit" name="signup" id="submit" class="btn btn-outline-primary btn-block" value="Signup">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+<script src="js/bootstrap.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/javascript.util/0.12.12/javascript.util.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(".input").focus(function() {
+        $(this).parent().addClass("focus");
+    })
+</script>
+</html>
